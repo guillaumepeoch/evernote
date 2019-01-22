3ffc9881abb06d02583dab8a23ddc280d63221ae
guillaumepeoch <guillaume.peoch@gmail.com>
2019-01-22 14:53:08 -0800

    Playing with react-rails

---
+++ app/assets/javascripts/components/navbar.jsx
@@ -0,0 +1,11 @@
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="/">Evernote</a>
+        </div>
+      </nav>
+    )
+  }
+}
--- app/assets/javascripts/components/test.jsx
+++
@@ -1,6 +0,0 @@
-// or, equivalent:
-class Test extends React.Component {
-  render() {
-    return <h1>Test</h1>
-  }
-}
