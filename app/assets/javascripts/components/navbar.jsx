82390088efcf904c3f894e72246c217a6797f2df
guillaumepeoch <guillaume.peoch@gmail.com>
2019-01-29 15:06:01 -0800

    side menu OK

--- app/assets/javascripts/components/navbar.jsx
+++ app/assets/javascripts/components/navbar.jsx
@@ -1,7 +1,7 @@
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="main-nav navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <a className="navbar-brand" href="/">Evernote</a>
         </div>
